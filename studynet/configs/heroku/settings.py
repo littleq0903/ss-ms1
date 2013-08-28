ead055d0bf8ba80f564d4c44ac5d90c13094d157
LittleQ <littleq0903@gmail.com>
2013-08-29 03:46:49 +0800

    debug

--- studynet/configs/heroku/settings.py
+++ studynet/configs/heroku/settings.py
@@ -5,12 +5,14 @@ from ..common.settings import *
 import dj_database_url
 
 DATABASES = {
-        'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COBALT_URL"])
+    'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COBALT_URL"])
 }
 
 HEROKU_APPS = (
     'gunicorn',
 )
+
+DEBUG = True
 
 
 INSTALLED_APPS += HEROKU_APPS
