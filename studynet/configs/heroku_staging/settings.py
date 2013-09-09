77b4098ce3814e389e7e1df0d942fa341962c458
LittleQ <littleq0903@gmail.com>
2013-09-09 21:52:14 +0800

    added staging branching

---
+++ studynet/configs/heroku_staging/settings.py
@@ -0,0 +1,17 @@
+import os
+
+from ..common.settings import *
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COBALT_URL"])
+}
+
+HEROKU_APPS = (
+    'gunicorn',
+)
+
+
+
+INSTALLED_APPS += HEROKU_APPS
